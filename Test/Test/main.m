f43da434ea322982bd9ce3f27d94a1ab155e311d
Zhang_Wei <1072231153@qq.com>
2015-03-28 17:19:16 +0800

    add Test

---
+++ Test/Test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Test
+//
+//  Created by qianfeng on 3/27/15.
+//  Copyright (c) 2015 张维. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
