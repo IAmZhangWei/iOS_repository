b5161f6fe842613e0964c8305b7fa7654d658b21
Zhang_Wei <1072231153@qq.com>
2015-03-28 19:12:20 +0800

    all

---
+++ 大众点评/大众点评/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  大众点评
+//
+//  Created by qianfeng on 3/28/15.
+//  Copyright (c) 2015 张维. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
