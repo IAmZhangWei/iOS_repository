e2801a09163a4eab89afb6a3173e13943399d36c
Zhang Wei <1072231153@qq.com>
2015-03-31 22:22:42 +0800

    new

---
+++ 大众点评/Controllers/NavC.h
@@ -0,0 +1,13 @@
+//
+//  NavC.h
+//  大众点评
+//
+//  Created by qianfeng on 3/29/15.
+//  Copyright (c) 2015 张维. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavC : UINavigationController
+
+@end
---
+++ 大众点评/Controllers/发现/DiscoveryVC.m
@@ -0,0 +1,38 @@
+//
+//  DiscoveryVC.m
+//  大众点评
+//
+//  Created by qianfeng on 3/29/15.
+//  Copyright (c) 2015 张维. All rights reserved.
+//
+
+#import "DiscoveryVC.h"
+
+@interface DiscoveryVC ()
+
+@end
+
+@implementation DiscoveryVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self.view setBackgroundColor:[UIColor redColor]];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
